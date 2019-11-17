33657699cc327d6615a1685d02a2dfdf44ca8c05
Usui11 <zchoeda101@gmail.com>
2019-11-17 23:17:50 +0530

    first commit

---
+++ js/bloglist.js
@@ -0,0 +1,7 @@
+function closeConfirm(){
+     document.getElementsByClassName("delete")[0].style.display="none";
+}
+
+function openConfirm(){
+     document.getElementsByClassName("delete")[0].style.display="block";
+}
---
+++ js/header.js
@@ -0,0 +1,31 @@
+function opensignUp(){
+    
+    document.getElementsByClassName("signup-C")[0].style.display="block";
+}
+
+function closesignUp(){
+   document.getElementsByClassName("signup-C")[0].style.display="none";
+}
+
+function opensignIn(){
+     document.getElementsByClassName("signin-C")[0].style.display="block";
+}
+
+function closesignIn(){
+     document.getElementsByClassName("signin-C")[0].style.display="none";
+}
+
+
+function modalSignUp(){
+    document.getElementsByClassName("signin-C")[0].style.display="none";
+    document.getElementsByClassName("signup-C")[0].style.display="block";
+}
+
+function opencreate(){
+     document.getElementsByClassName("create")[0].style.display="block";
+}
+
+function closecreate(){
+     document.getElementsByClassName("create")[0].style.display="none";
+}
+
